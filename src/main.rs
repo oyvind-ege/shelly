485b981fcb9bac72990b3bfbcb497466b9d8b4e0
Øyvind Ege <oyvind.ege@pm.me>
2025-01-07 14:13:18 +0100

    Added infinite loop

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,16 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
-    println!("{}: command not found", input.trim());
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+        println!("{}: command not found", input.trim());
+    }
 }
 
 fn validate_command(cmd: &str) -> bool {
