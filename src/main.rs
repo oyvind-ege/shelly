e796a24714a12157a3562c5bbf3b1b3335fe36ea
Øyvind Ege <oyvind.ege@pm.me>
2025-01-07 14:01:59 +0100

    Command not found: ugly

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,20 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+    println!("{}: command not found \n", input);
+}
+
+fn validate_command(cmd: &str) -> bool {
+    false
+}
+
+#[cfg(test)]
+mod tests {
+    use super::validate_command;
+
+    #[test]
+    fn test_basic() {
+        assert!(!validate_command("test"));
+        assert!(!validate_command("invalid command"));
+    }
 }
