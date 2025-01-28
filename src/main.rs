70713305c89023231344a4fcb32e6327f42487eb
Øyvind Ege <oyvind.ege@pm.me>
2025-01-28 11:31:33 +0100

    Starting to get somewhere

--- src/main.rs
+++ src/main.rs
@@ -3,50 +3,89 @@ use std::process::{self};
 
 extern crate exitcode;
 
+trait Execute {
+    fn execute(&self);
+}
+
 #[derive(Debug)]
 enum CommandKind {
-    Exit,
-    Echo,
+    Type(TypeCommand),
+    Exit(ExitCommand),
+    Echo(EchoCommand),
     Invalid(String),
 }
 
 #[derive(Debug)]
 struct Command<'a> {
-    cmd: CommandKind,
+    cmd: String,
     args: Vec<&'a str>,
+}
+
+#[derive(Debug)]
+struct ExitCommand {
+    args: Vec<String>,
+}
+#[derive(Debug)]
+struct EchoCommand {
+    args: Vec<String>,
+}
+#[derive(Debug)]
+struct TypeCommand {}
+
+#[derive(Debug)]
+struct InvalidCommand {
+    args: Vec<String>,
 }
 
 impl<'a> Command<'a> {
     fn new() -> Self {
         Command {
-            cmd: CommandKind::Invalid(String::from("none")),
+            cmd: "Invalid".to_string(),
             args: vec![""],
         }
     }
 
-    fn parse(&mut self, input: &'a str) -> &mut Self {
-        let split_input = input.split_whitespace().collect::<Vec<&'a str>>();
+    fn parse(&mut self, input: String) -> Box<dyn Execute> {
+        let split_input = input.split_whitespace().collect::<Vec<&str>>();
+
+        self.args = if split_input.len() > 1 {
+            split_input[1..].to_vec()
+        } else {
+            [""].to_vec()
+        };
+
         match split_input[0] {
-            "exit" => self.cmd = CommandKind::Exit,
-            "echo" => self.cmd = CommandKind::Echo,
-            cmd => self.cmd = CommandKind::Invalid(String::from(cmd)),
-        };
-
-        if split_input.len() > 1 {
-            self.args = split_input[1..].to_vec();
-        };
-        self
-    }
-
-    fn execute(&self) {
-        match &self.cmd {
-            CommandKind::Exit if self.args.first().unwrap() == &"0" => process::exit(exitcode::OK),
-            CommandKind::Echo => {
-                println!("{}", self.args.join(" "));
-            }
-            CommandKind::Exit => process::exit(exitcode::SOFTWARE),
-            CommandKind::Invalid(cmd) => println!("{}: command not found", cmd),
+            "exit" => Box::new(ExitCommand {
+                args: self.args.clone(),
+            }),
+            "echo" => Box::new(EchoCommand {
+                args: self.args.clone(),
+            }),
+            cmd => Box::new(InvalidCommand {
+                args: [cmd.to_string()].to_vec(),
+            }),
         }
+    }
+}
+
+impl Execute for ExitCommand {
+    fn execute(&self) {
+        match self.args.first().unwrap() {
+            val if val == "0" => process::exit(exitcode::OK),
+            _ => process::exit(exitcode::USAGE),
+        }
+    }
+}
+
+impl Execute for EchoCommand {
+    fn execute(&self) {
+        println!("{}", self.args.join(" "));
+    }
+}
+
+impl Execute for InvalidCommand {
+    fn execute(&self) {
+        println!("{:?}: command not found", self.args);
     }
 }
 
@@ -59,8 +98,7 @@ fn main() {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-        let mut command: Command = Command::new();
-        command.parse(input.trim()).execute();
+        Command::new().parse(input.trim().to_string()).execute();
     }
 }
 
