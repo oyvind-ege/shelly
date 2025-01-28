f8131ed4b3199076d58b704296e3292ddc0a5bfc
Øyvind Ege <oyvind.ege@pm.me>
2025-01-28 09:42:06 +0100

    Added support for echo

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,56 @@
-#[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::{self};
+
+extern crate exitcode;
 
 #[derive(Debug)]
-enum ProgramError {
-    IOError(std::io::Error),
+enum CommandKind {
+    Exit,
+    Echo,
+    Invalid(String),
 }
 
-// impl<'a> Command<'a> {
-//     fn parse(input: String) -> Self {
-//         // From "exit 0" to "Command { cmd: "exit", args: vec![0]}"
-//         let split_string: Vec<&str> = input.split_whitespace().collect::<Vec<&str>>();
-//         let command = Command {
-//             raw_cmd: input.clone(),
-//             cmd: split_string[0],
-//             args: split_string[1],
-//         };
-//         command
-//     }
-// }
+#[derive(Debug)]
+struct Command<'a> {
+    cmd: CommandKind,
+    args: Vec<&'a str>,
+}
 
-fn main() -> Result<(), ProgramError> {
+impl<'a> Command<'a> {
+    fn new() -> Self {
+        Command {
+            cmd: CommandKind::Invalid(String::from("none")),
+            args: vec![""],
+        }
+    }
+
+    fn parse(&mut self, input: &'a str) -> &mut Self {
+        let split_input = input.split_whitespace().collect::<Vec<&'a str>>();
+        match split_input[0] {
+            "exit" => self.cmd = CommandKind::Exit,
+            "echo" => self.cmd = CommandKind::Echo,
+            cmd => self.cmd = CommandKind::Invalid(String::from(cmd)),
+        };
+
+        if split_input.len() > 1 {
+            self.args = split_input[1..].to_vec();
+        };
+        self
+    }
+
+    fn execute(&self) {
+        match &self.cmd {
+            CommandKind::Exit if self.args.first().unwrap() == &"0" => process::exit(exitcode::OK),
+            CommandKind::Echo => {
+                println!("{}", self.args.join(" "));
+            }
+            CommandKind::Exit => process::exit(exitcode::SOFTWARE),
+            CommandKind::Invalid(cmd) => println!("{}: command not found", cmd),
+        }
+    }
+}
+
+fn main() {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -28,9 +59,18 @@ fn main() -> Result<(), ProgramError> {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-        match input.trim() {
-            "exit 0" => return Ok(()),
-            n => println!("{}: command not found", n),
-        };
+        let mut command: Command = Command::new();
+        command.parse(input.trim()).execute();
     }
+}
+
+// command.parse(input.trim()).execute();
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn it_works() {}
 }
